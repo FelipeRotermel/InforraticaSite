32fc78f3a447d0fa46f64e43c0201d42779e1a00
paulozemr <pauloramosj213@gmail.com>
2023-05-26 14:53:18 -0300

    Inicializando a tela de login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,31 +2,32 @@
 
 <template>
   <section class="text-center">
-    <div class="p-5 bg-image" style="background-image: url('https://mdbootstrap.com/img/new/textures/full/171.jpg'); height: 300px;"></div>
-    <div class="card mx-4 mx-md-5 shadow-5-strong" style="margin-top: -100px;background: hsla(0, 0%, 100%, 0.8);backdrop-filter: blur(30px);">
+    <div class="p-5 bg-image" style="background-image: url('https://mdbootstrap.com/img/new/textures/full/171.jpg'); height: 350px;">
+    </div>
+    <div class="card mx-5 mx-md-5 shadow-5-strong" style="margin-top: -100px;background: hsla(0, 0%, 100%, 0.8);backdrop-filter: blur(30px);">
       <div class="card-body py-5 px-md-5">
         <div class="row d-flex justify-content-center">
           <div class="col-lg-8">
             <h2 class="fw-bold mb-5">Login</h2>
             <form>
               <div class="row">
-                <div class="form-outline mb-4">
-                  <input type="text" id="form3Example1" class="form-control" />
-                  <label class="form-label" for="form3Example1">First name</label>
+                <div class="form-outline mb-5">
+                  <input type="text" id="form3Example1" placeholder="Nome ou Email" class="form-control" />
                 </div>
               </div>
 
               <!-- Password input -->
               <div class="form-outline mb-4">
-                <input type="password" id="form3Example4" class="form-control" />
-                <label class="form-label" for="form3Example4">Password</label>
+                <input type="password" id="form3Example4" placeholder="Senha" class="form-control" />
               </div>
 
               <!-- Submit button -->
-              <button type="submit" class="btn btn-primary btn-block mb-4">
-                Sign up
+              <button type="submit" class="btn btn-outline-secondary btn-lg active mb-4">
+                Entre
               </button>
+
             </form>
+            <a href="#" class="btn btn-outline-info btn-lg" role="button" aria-pressed="true">Crie uma conta</a>
           </div>
         </div>
       </div>

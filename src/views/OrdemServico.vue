3512d73c5a4982a01db7880434e4bb7d543d337c
paulozemr <pauloramosj213@gmail.com>
2023-05-26 17:54:32 -0300

    Conceitualização da pagina de O.S

--- src/views/OrdemServico.vue
+++ src/views/OrdemServico.vue
@@ -1,5 +1,86 @@
 <script></script>
 
-<template></template>
+<template>
+  <div class="container-fluid">
+    <div class="col-12 text-center">
+      <div class="title">
+        <h2>Ordem de serviço</h2>
+      </div>
+    </div>
+  </div>    
+      <div
+    class="card p-5 mb-3"
+    style="width: 1000px; margin: 50px; max-width: max-content"
+  >
+    <div class="row g-0">
+      <div class="col-md-4">
+        <img
+          src="https://m.media-amazon.com/images/I/519CpLs066L._AC_UF894,1000_QL80_.jpg"
+          class="img-fluid rounded-start"
+          alt="..."
+        />
+      </div>
+      <div class="col-md-8">
+        <div class="card-body">
+          <h5 class="card-title">Card do pc</h5>
+          <table class="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Peça</th>
+                <th scope="col">Peça</th>
+                <th scope="col">Peça</th>
+                <th scope="col">Peça</th>
+                <th scope="col">Peça</th>
+                <th scope="col">Peça</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>PEÇA ALEATORIA</th>
+                <th>PEÇA ALEATORIA</th>
+                <th>PEÇA ALEATORIA</th>
+                <th>PEÇA ALEATORIA</th>
+                <th>PEÇA ALEATORIA</th>
+                <th>PEÇA ALEATORIA</th>
+              </tr>
+            </tbody>
+          </table>
+          <div class="btn-group" role="group">
+            <button class="btn btn-danger btn-sm">
+              Excluir</button
+            ><button class="btn btn-danger btn-sm">
+              Excluir</button
+            ><button class="btn btn-danger btn-sm">
+              Excluir</button
+            ><button class="btn btn-danger btn-sm">
+              Excluir
+            </button>
+          </div>
+          <div class="btn-group" role="group">
+            <button class="btn btn-info btn-sm">
+              editar</button
+            ><button class="btn btn-info btn-sm">
+              editar</button
+            ><button class="btn btn-info btn-sm">
+              editar</button
+            ><button class="btn btn-info btn-sm">
+              editar
+            </button>
+          </div>
+          <p class="card-text">
+            <small class="text-muted"
+              >informaçoes minimas e exclusivas do pc</small
+            >
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 
-<style scoped></style>
+<style scoped>
+.input {
+  margin: 10px;
+  justify-content: center;
+}
+</style>

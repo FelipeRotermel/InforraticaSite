ee8f7a69f059b6a6f9c45ec7d64111467f2ee269
FelipeRotermel <feliperotermell@gmail.com>
2023-10-19 23:14:53 -0300

    teste

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '@/views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,32 +12,32 @@ const router = createRouter({
     {
       path: '/login',
       name: 'login',
-      component: () => import('../views/Login.vue')
+      component: () => import('@/views/Login.vue')
     },
     {
       path: '/ordem-servico',
       name: 'ordem-servico',
-      component: () => import('../views/OrdemServico.vue')
+      component: () => import('@/views/OrdemServico.vue')
     },
     {
       path: '/cadastro-computador',
       name: 'cadastro-computador',
-      component: () => import('../views/CadastroComputador.vue')
+      component: () => import('@/views/CadastroComputador.vue')
     },
     {
       path: '/cadastro-notebook',
       name: 'cadastro-notebook',
-      component: () => import('../views/CadastroNotebook.vue')
+      component: () => import('@/views/CadastroNotebook.vue')
     },
     {
       path: '/cadastro-usuario',
       name: 'cadastro-usuario',
-      component: () => import('../views/CadastroUsuario.vue')
+      component: () => import('@/views/CadastroUsuario.vue')
     },
     {
       path: '/cadastro-ordem-servico',
       name: 'cadastro-ordem-servico',
-      component: () => import('../views/CadastroOrdemServico.vue')
+      component: () => import('@/views/CadastroOrdemServico.vue')
     },
   ]
 })
